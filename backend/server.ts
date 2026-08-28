a1ce932a4c6e2d8ef5a8087e59255fb95669e514
NivedithaDevang <niveditha8277@gmail.com>
2026-08-28 15:32:31 +0530

    add authentication login route

--- backend/server.ts
+++ backend/server.ts
@@ -5,13 +5,11 @@ import cityRouter from "./routes/cityRoute.js";
 import categoryRouter from "./routes/categoryRoute.js";
 import orgReqRouter from "./routes/orgRequestRoute.js";
 import organizerRouter from "./routes/organiserRoute.js";
-<<<<<<< HEAD
 import eventRouter from "./routes/eventRoute.js";
 import bookRouter from "./routes/bookingRoute.js";
 import passRouter from "./routes/passRoute.js";
-=======
+
 import authRoutes from "./routes/authRoute.js";
->>>>>>> validator
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -30,14 +28,11 @@ app.use("/cities", cityRouter);
 app.use("/categories", categoryRouter);
 app.use("/orgreq", orgReqRouter);
 app.use("/organizers", organizerRouter);
-<<<<<<< HEAD
 app.use("/events", eventRouter);
 app.use("/bookings", bookRouter);
 app.use("/passes", passRouter);
 
-=======
 app.use("/auth", authRoutes);
->>>>>>> validator
 
 const PORT = process.env.PORT || 5000;
 
